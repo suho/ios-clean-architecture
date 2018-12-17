be576a9d95b7d3cb82923987730876c498a0e9d2
Su Ho V <hovansu.it@gmail.com>
2018-12-17 16:55:09 +0700

    update ci

--- CleanArchitectureTests/CleanArchitectureTests.swift
+++ CleanArchitectureTests/CleanArchitectureTests.swift
@@ -9,4 +9,25 @@
 import XCTest
 @testable import CleanArchitecture
 
-class CleanArchitectureTests: XCTestCase {}
+class CleanArchitectureTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+}
